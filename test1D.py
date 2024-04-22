63dbe6daac56c8e4de449611862b937e9eb6e1d0
schiesser <schiesser@LAPTOP-AV5KAO6S>
2024-04-22 11:24:01 +0200

    integration gradient

--- test1D.py
+++ test1D.py
@@ -49,21 +49,39 @@ nodal_field[2,0]=1
 '''
 ### Cas d'un champ vectoriel :
 nodal_vector=np.ones((nodes.shape[0],nodes.shape[1]))*3
-nodal_vector[0,0]=2 #première coordonnée : numérotation du noeud; deuxième coordonnée : selon dimensions (ici champ 2D)
-nodal_vector[2,0]=1
+#nodal_vector[0,0]=2 #première coordonnée : numérotation du noeud; deuxième coordonnée : selon dimensions (ici champ 2D)
+#nodal_vector[2,0]=1
 
-'''print("nodal_field testé :")
-print(nodal_field)
+'''
+print("nodal_field testé :")
+print(nodal_vector)
 print("avec les connections :")
-print(conn)'''
-
-
-# Gradient 
+print(conn)
+'''
 
 NTF2 = NodalTensorField("displacement2", Sup, nodal_vector)
 
 
+## gradient deplacement
 
-'''print(dir(Sup.fem.getShapesDerivatives(Sup.elem_type)))
+
+field_dim = nodes.shape[1]
+nb_element = mesh.getConnectivity(Sup.elem_type).shape[0]
+nb_integration_points = Sup.fem.getNbIntegrationPoints(Sup.elem_type)
+result_integration = np.zeros((nb_element*nb_integration_points, field_dim ))
+nodes_per_elem=2
+
+n = nodal_vector[conn].reshape((nodes_per_elem,nb_element))
+der = Sup.fem.getShapesDerivatives(Sup.elem_type)
+
+gradquapoint = np.sum(n*der,axis=1)
+
+
+gradquapoint =gradquapoint.reshape((nb_integration_points*nb_element,field_dim))
+Sup.fem.integrate(gradquapoint,result_integration,field_dim, Sup.elem_type)
+
+print(np.sum(result_integration,axis=0))
+'''
+print(dir(Sup.fem.getShapesDerivatives(Sup.elem_type)))
 print(Sup.fem.getShapesDerivatives(Sup.elem_type))
 '''
