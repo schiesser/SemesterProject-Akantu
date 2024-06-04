ecb85784ed5a5483f6f5156f3f3391bc6fc1858f
schiesser <schiesser@LAPTOP-AV5KAO6S>
2024-06-04 14:43:18 +0200

    correction avec parametre dim

--- plot.py
+++ plot.py
@@ -5,11 +5,12 @@ import subprocess
 import os
 import matplotlib.tri as tri
 from matplotlib.collections import LineCollection
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
 def readMesh(filename, element_type):
     mesh = meshio.read(filename)
     return mesh.points[:, :2], mesh.cells_dict[element_type]
-
+"""
 def plotMesh(coords, connectivity):
     plt.axes().set_aspect('equal')
     for segment in connectivity:
@@ -20,10 +21,11 @@ def plotMesh(coords, connectivity):
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.savefig('mesh_segment2.png')
+"""
 
 def meshGeo(filename, dim=2, order=1, element_type='line'):
     out = os.path.splitext(filename)[0] + '.msh'
-    ret = subprocess.run(f"gmsh -2 -order {order} -o {out} {filename}", shell=True)
+    ret = subprocess.run(f"gmsh -{dim} -order {order} -o {out} {filename}", shell=True)
     if ret.returncode:
         print("Beware, gmsh could not run: mesh is not generated")
     else:
@@ -59,8 +61,8 @@ def plotMesht3(coords, connectivity, nodal_field=None, title = None, name_file =
         contour = plt.tricontourf(triangles, nodal_field)
         plt.colorbar(contour)
 
-    plt.xlabel('X coordinates')
-    plt.ylabel('Y coordinates')
+    plt.xlabel('X')
+    plt.ylabel('Y')
     if title is not None :
         plt.title(title)
 
@@ -84,7 +86,7 @@ def plotMeshs(coords, connectivity, nodal_field=None, title=None, name_file="Mes
         # Créer une collection de lignes
         lc = LineCollection(segments, cmap='viridis', array=values.mean(axis=1), linewidths=2)
         plt.gca().add_collection(lc)
-        plt.colorbar(lc)
+        plt.colorbar(lc, fraction=0.025, pad=0.05)
     
     plt.scatter(coords[:, 0], coords[:, 1], color='black', s=10)
 
@@ -95,3 +97,28 @@ def plotMeshs(coords, connectivity, nodal_field=None, title=None, name_file="Mes
     
     plt.savefig(name_file)
     plt.close()
+
+def plotMeshtetra(nodes, conn):
+    
+    fig = plt.figure(figsize=(20, 20))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    edges = []
+    for tetra in conn:
+        edges.extend([[nodes[tetra[0]], nodes[tetra[1]]],[nodes[tetra[0]], nodes[tetra[2]]],[nodes[tetra[0]], nodes[tetra[3]]],[nodes[tetra[1]], nodes[tetra[2]]],[nodes[tetra[1]], nodes[tetra[3]]],[nodes[tetra[2]], nodes[tetra[3]]]])
+    
+    edge_collection = Line3DCollection(edges, colors='k', linestyles='--', linewidths=0.8)
+    ax.add_collection3d(edge_collection)
+    
+    ax.scatter(nodes[:, 0], nodes[:, 1], nodes[:, 2], color='black', s=10)
+    
+    # Set the labels
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # Set the aspect ratio to be equal
+    ax.set_box_aspect([np.ptp(nodes[:, 0]), np.ptp(nodes[:, 1]), np.ptp(nodes[:, 2])])
+
+    # Save and show the plot
+    plt.savefig('MeshElementTetrahedron.png')
